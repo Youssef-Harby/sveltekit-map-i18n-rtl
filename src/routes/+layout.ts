7ef772a070c91cd212d76cdb640770a105b9d365
Youssef-Harby <youssef_harby@yahoo.com>
2023-08-06 01:06:59 +0300

    back to dynamic route

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -16,5 +16,3 @@ export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale } }
 	// pass locale to the "rendering context"
 	return { locale };
 };
-
-export const prerender = true;
